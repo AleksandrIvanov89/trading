bbc54bbcac9949e22cc58f51dcc696259a3c1b41
AleksandrIvanov89 <axeliandr@protonmail.com>
2022-04-18 18:41:46 +0200

    scheduler for ohlcv writer

--- algorithm/ohlcv_writer_service/main.py
+++ algorithm/ohlcv_writer_service/main.py
@@ -1,9 +1,9 @@
 import os
 import json
 import time
+import schedule
 
 from exchanges_db import *
-
 
 def load_config():
     with open('config.json') as json_file:
@@ -16,17 +16,40 @@ def load_config():
 def main():
     exchange_name, symbol = load_config()
     #os.environ.get("FIREBASE_CREDENTIALS_PATH"), os.environ.get("FIREBASE_BOT_ID")
-    mongo_username = os.environ.get("MONGO_USERNAME")
-    mongo_password = os.environ.get("MONGO_PASSWORD")
-    rest_api_user = os.environ.get("REST_API_USER")
-    rest_api_password = os.environ.get("REST_API_PASSWORD")
-    rest_api_base_url = os.environ.get("REST_API_BASE_URL")
-    data_service_api = DataServiceAPI(
-        rest_api_base_url, rest_api_user, rest_api_password)
-    mongodb = MongoDB(exchange_name, symbol, mongo_username,
-                      mongo_password, "mongodb:27017", data_service_api)
 
-    mongodb.update_ohlcv_loop()
+    data_service_api = DataServiceAPI(
+        os.environ.get("REST_API_BASE_URL"),
+        os.environ.get("REST_API_USER"),
+        os.environ.get("REST_API_PASSWORD")
+        )
+
+    mongodb = MongoDB(
+        exchange_name,
+        symbol,
+        os.environ.get("MONGO_USERNAME"),
+        os.environ.get("MONGO_PASSWORD"),
+        "mongodb:27017",
+        data_service_api
+        )
+
+    mongodb.update_ohlcvs_all_periods()
+
+    schedule.every().minute.at(":20").do(
+        mongodb.update_ohlcv,
+        period='1m'
+        )
+    schedule.every().hour.at(":01").do(
+        mongodb.update_ohlcv,
+        period='1h'
+        )
+    schedule.every().day.at("00:01").do(
+        mongodb.update_ohlcv,
+        period='1d'
+        )
+    
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
 
 if __name__ == '__main__':
